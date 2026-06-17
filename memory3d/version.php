00f47b7a38f11b25422b90228a2a137f2b47f294
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2026-06-17 15:09:44 -0300

    Bump version to 2026061700

--- memory3d/version.php
+++ memory3d/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_memory3d';
-$plugin->version   = 2026051800;
+$plugin->version   = 2026061700;
 $plugin->requires  = 2022111800;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
